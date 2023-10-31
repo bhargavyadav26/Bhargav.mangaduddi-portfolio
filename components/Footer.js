ebdd2ceaa2f3b1acaefc4bb58c8ff18bb6131825
bhargavyadav26 <bhargavyadav26@gmail.com>
2023-10-30 21:07:19 -0400

    responsive changes for mobile

--- components/Footer.js
+++ components/Footer.js
@@ -18,7 +18,7 @@ export default function Footer(props) {
     return (
         <div style={{marginTop: '15px'}}>
             <Grid>
-                <Grid.Row columns={3}>
+                <Grid.Row columns={3} only="computer tablet">
                     <Grid.Column textAlign="center" verticalAlign="middle">
                         <span style={{fontSize:"medium", fontWeight: "bold", color: textColor  }} aria-label="copyright bhargav mangaduddi">
                         &copy; 2023 Bhargav Mangaduddi. All Rights Reserved.
@@ -29,6 +29,51 @@ export default function Footer(props) {
 
                     </Grid.Column>
                     <Grid.Column>
+                        <Grid  textAlign="center" verticalAlign="middle">
+                            <Grid.Row>
+                            <Image src='/twitterx.png'
+                            as="a"
+                            alt="twitter"
+                            href='https://twitter.com/bhargav_yadav'
+                            target='_blank' />
+                    <Image src='/linkedin.png'
+                    as="a"
+                    alt="linkedin"
+                    href='https://www.linkedin.com/in/bhargavmangaduddi'
+                    target='_blank'
+                     />
+                         <Image src='/instagram.png'
+                    as="a"
+                    alt="instagram"
+                    href='https://www.instagram.com/bhargav.yadav/'
+                    target='_blank'
+                     />
+                     <Image src='/github.png'
+                as="a"
+                alt="github"
+                href='https://github.com/bhargavyadav26'
+                target='_blank'
+                 />
+                            </Grid.Row>
+                            </Grid>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row only="mobile" style={{padding: 0, margin: 0}}>
+                    <Grid.Column textAlign="center" verticalAlign="middle">
+                        <span style={{fontSize:"medium", fontWeight: "bold", color: textColor  }} aria-label="copyright bhargav mangaduddi">
+                        &copy; 2023 Bhargav Mangaduddi. All Rights Reserved.
+                        </span>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row only="mobile">
+                <Grid.Column textAlign="center" verticalAlign="middle">
+                    <ContactMe isModalOpen={props.isModalOpen} setIsModalOpen={props.setIsModalOpen} />
+
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row only="mobile" style={{padding: 0, margin: 0}}>
+                    
+                <Grid.Column>
                         <Grid  textAlign="center" verticalAlign="middle">
                             <Grid.Row>
                             <Image src='/twitterx.png'
--- components/Header.js
+++ components/Header.js
@@ -15,13 +15,8 @@ export default function Header(props) {
     
     return (
         <Grid style={{paddingTop: "1rem"}}>
-            <Grid.Row columns={3}>
+            <Grid.Row columns={2} only='computer tablet'>
                 <Grid.Column/>
-                <Grid.Column>
-                    {/* <span style={{color: isNightMode ? 'white' : 'black'}}>
-                    Welcome
-                    </span> */}
-                </Grid.Column>
                 <Grid.Column textAlign='center' verticalAlign='middle'>
                     <div style={{display: "flex", flexDirection: "row-reverse"}}>
       &nbsp;&nbsp;
@@ -58,6 +53,44 @@ export default function Header(props) {
     </div>
                 </Grid.Column>
             </Grid.Row>
+            <Grid.Row columns={1} only='mobile'>
+                <Grid.Column  verticalAlign='middle'>
+                    <div style={{display: "flex", flexDirection: "row-reverse", justifyContent: 'center'}}>
+      &nbsp;&nbsp;
+      &nbsp;&nbsp;
+      &nbsp;&nbsp;
+      &nbsp;&nbsp;
+                <Popup
+        content="This website is ADA compliance. Turn on narrator or voice over."
+        trigger={<Image alt="This website is ADA compliance. Turn on narrator or voice over." src="/ada.png" size="mini" />}
+      />
+      &nbsp;&nbsp;
+      &nbsp;&nbsp;
+                <div>
+      <input
+        type="checkbox"
+        id="custom-checkbox"
+        className="custom-checkbox"
+        checked={!isNightMode}
+        onChange={toggle}
+        aria-hidden="true"
+      />
+      <label htmlFor="custom-checkbox" className="custom-label">
+        <img
+          src={isNightMode ? '/sun.png' : '/moon.png'}
+          alt={isNightMode ? 'Day' : 'Night'}
+          className="custom-image"
+        />
+      </label>
+    </div>
+    &nbsp;&nbsp;
+      &nbsp;&nbsp;
+      {/* <Button basic onClick={() => setIsModalOpen(!isModalOpen)}><span style={{fontWeight: 'bolder', color: isNightMode ? 'white':'black'}}>Contact Me</span></Button> */}
+      <ContactMe isModalOpen={props.isModalOpen} setIsModalOpen={props.setIsModalOpen} />
+    </div>
+                </Grid.Column>
+            </Grid.Row>
+            
         </Grid>
     )
 }
